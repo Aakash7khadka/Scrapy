ccdde2f666e57dc2c32568831c2b97d3e89e0c49
Aakash7khadka <aakash7khadka@gmail.com>
2020-07-07 13:08:45 +0545

    first commit

---
+++ quote_tutorial/quote_tutorial/spiders/quote_spider.py
@@ -0,0 +1,27 @@
+
+import scrapy
+
+from .. items import QuoteTutorialItem
+
+class quote_spider(scrapy.Spider):
+    page_number=2
+    name='quote_spider'
+    start_urls=[
+        'http://quotes.toscrape.com/'
+    ]
+    def parse(self,response):
+        item=QuoteTutorialItem()
+        # title=response.css('title::text').extract()
+        all_quotes=response.css('.quote')
+        for quote in all_quotes:
+            quotes=quote.css('span.text::text').extract()
+            author=quote.css('.author::text')[0].extract()
+            tags=quote.css('.tags .tag::text').extract()
+            item['quote']=quotes
+            item['author']=author
+            item['tags']=tags
+            yield item
+
+        next_page='http://quotes.toscrape.com/page/'+str(quote_spider.page_number)+'/' 
+        if quote_spider.page_number<11:
+            yield response.follow(next_page,callback=self.parse)
